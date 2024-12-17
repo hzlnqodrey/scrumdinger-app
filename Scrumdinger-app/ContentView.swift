0082312f06969065f2dcbfa2af7c8ec83165fc76
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-17 21:10:43 +0700

    Initial Commit

---
+++ Scrumdinger-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdinger-app
+//
+//  Created by Hazlan Muhammad Qodri on 17/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdinger-app/Scrumdinger_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_appApp.swift
+//  Scrumdinger-app
+//
+//  Created by Hazlan Muhammad Qodri on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
