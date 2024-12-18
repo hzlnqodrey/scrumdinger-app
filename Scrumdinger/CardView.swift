4998d2078cb593ff45438f32c9b2be28d00459eb
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-18 18:24:46 +0700

    Initial Commit

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Hazlan Muhammad Qodri on 18/12/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CardView()
+}
