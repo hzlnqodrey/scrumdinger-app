ca225690ec788b394841b4fb6661754f59a67979
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-18 18:13:55 +0700

    create DailyScrum struct

--- CreatingACardView/StartingProject/Scrumdinger/Models/DailyScrum.swift
+++ CreatingACardView/StartingProject/Scrumdinger/Models/DailyScrum.swift
@@ -13,3 +13,12 @@ struct DailyScrum {
     var lengthInMinute: Int
     var theme: Theme
 }
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Caty", "Hazlan", "Daisy", "Simon", "Erza", "Jon"], lengthInMinute: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Demon", "Natsu", "Beast", "Wilden"], lengthInMinute: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Karla", "Eden", "Aga", "Lindsey", "Chad", "Jenn", "Sarah"], lengthInMinute: 10, theme: .poppy)
+    ]
+}
