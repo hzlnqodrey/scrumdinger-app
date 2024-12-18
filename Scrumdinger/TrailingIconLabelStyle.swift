5a712c4770c15a89a448cb7788994d8ec9f0af6b
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-19 02:19:47 +0700

    finishing creating a card view, next up make a listview for card

--- Scrumdinger/TrailingIconLabelStyle.swift
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -5,4 +5,17 @@
 //  Created by Hazlan Muhammad Qodri on 18/12/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
