7bdb65851800292b35eb0046bf99c4c9b34ffa5b
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-19 19:29:22 +0700

    create list in detail view

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -5,4 +5,37 @@
 //  Created by Hazlan Muhammad Qodri on 19/12/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct DetailView: View {
+    var scrum: DailyScrum
+    
+    var body: some View {
+        
+        List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "hourglass")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) Minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpallete.fill")
+                }
+            }
+        }
+        
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[1])
+        }
+    }
+}
