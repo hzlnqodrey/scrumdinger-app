4166759a0d50932c0ede9e4ddf62d28df66b65fb
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-19 15:22:53 +0700

    added card's color to list view

---
+++ Scrumdinger/Models/ScrumView.swift
@@ -0,0 +1,8 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by Hazlan Muhammad Qodri on 19/12/24.
+//
+
+import Foundation
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumView.swift
+//  Scrumdinger
+//
+//  Created by Hazlan Muhammad Qodri on 19/12/24.
+//
+
+import SwiftUI
+
+struct ScrumView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        Text("ScrumView")
+    }
+}
+
+struct SrcumView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumView()
+    }
+}
