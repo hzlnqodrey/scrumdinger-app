9d3fd04e81704ab0165743821aa79a92b2ed585b
Hazlan Qodrey <hazlanmuhammadqodri3@gmail.com>
2024-12-26 20:19:22 +0700

    build edit detail scrum

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct DetailView: View {
-    var scrum: DailyScrum
+    let scrum: DailyScrum
+    
+    @State private var isPresentingEditView = false
     
     var body: some View {
         
@@ -45,6 +47,31 @@ struct DetailView: View {
         }
         .navigationTitle(scrum.title)
         .padding(1)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
 
